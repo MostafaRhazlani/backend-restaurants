e0cac8000734cc4b8f657846494ab26e1a8259ec
MostafaRhazlani <mo.rhazlani@gmail.com>
2024-02-05 16:46:23 +0100

    cretae -mc and database menus

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menu>
+ */
+class MenuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name_menu' => fake()->name(),
+            'price' => fake()->numberBetween(5, 300),
+            'description' => fake()->paragraph(),
+            'category_id' => Category::all()->random()->id,
+        ];
+    }
+}
