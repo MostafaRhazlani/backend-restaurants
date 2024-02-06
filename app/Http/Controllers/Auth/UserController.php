9588c4009881c21d26ab7ac3781833ec066b3fe0
MostafaRhazlani <mo.rhazlani@gmail.com>
2024-02-07 00:09:52 +0100

    use sanctum and create login

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function index() {
+        return Auth::user();
+    }
 
     public function login(Request $request) {
         $fields = $request->validate([
@@ -26,12 +31,31 @@ class UserController extends Controller
 
         $token = $user->createToken('myapptoken')->plainTextToken;
 
-        $response = [
+        return $response = [
             'user' => $user,
             'token' => $token
         ];
+    }
 
-        return response($response, 201);
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+            'role_id' => 3
+        ]);
+
+        $response = [
+            'user' => $user
+        ];
+
+         response($response, 201);
     }
 
     public function logout() {
